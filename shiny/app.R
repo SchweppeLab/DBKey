2fcd876ee3136f3c5dcafa72639fbbd12df0d080
William Barshop <wbarshop@ucla.edu>
2021-10-12 15:14:29 -0700

    Wrap in dashboard (is this needed? looks nicer, though...)

--- shiny/app.R
+++ shiny/app.R
@@ -9,6 +9,7 @@
 
 library(shiny)
 library(shinybusy)
+library(shinydashboard)
 library(RSQLite)                                                                                                                                                                                          
 library(stringr)                                                                                                                                                                                          
 library(Rcpp)                                                                                                                                                                                             
@@ -31,22 +32,42 @@ options(shiny.maxRequestSize = 300*1024^2)
 options(shiny.port = 3838)
 options(shiny.host = "0.0.0.0")
 
-ui <- fluidPage(
-    #MSPtoDB(Library, FragmentationMode, MassAnalyzer, CollisionEnergy, TMTPro, DBoutput, Source, topX, cutoff)
-    
-    # Application title
-    titlePanel("Library Maker"),
-    fileInput("LibInput", "Input Files", accept=c('.msp','.MSP','.sptxt'), multiple = TRUE),
-    selectInput("FragInput", "Fragmentation mode", c("HCD", "CID")),
-    numericInput("CeInput", "Collision Energy", 35, min =0, max = 00),
-    selectInput("MassAnalyzerInput", "Mass Analyzer", c("IT", "OT")),
-    selectInput("TMTProInput", "Add TMTPro?", c("TRUE", "FALSE")),
-    selectInput("SourceInput", "Source", c("Prosit", "SpectraST")),
-    numericInput("topX", "Top N Peaks only", 150),
-    numericInput("cutoff", "% intensity cutoff", 0),
-    downloadButton(label = "Generate and Download .db", "downloadData"),
-    add_busy_spinner(spin = "fading-circle")
+header <- dashboardHeader(
+  title = "Library Maker"
 )
+
+side <- dashboardSidebar(
+          sidebarMenu(
+             menuItem("Library Maker", tabName = "maker", icon = icon("dashboard"))
+          )
+      )
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "maker",
+      fluidPage(
+        titlePanel("Spectral Library to mzVault .db Converter"),
+		fileInput("LibInput", "Input Files", accept=c('.msp','.MSP','.sptxt'), multiple = TRUE),
+        selectInput("FragInput", "Fragmentation mode", c("HCD", "CID")),
+        numericInput("CeInput", "Normalized Collision Energy (NCE)", 35, min =0, max = 00),
+        selectInput("MassAnalyzerInput", "Mass Analyzer", c("OT", "IT")),
+        selectInput("TMTProInput", "Add TMTPro?", c("FALSE", "TRUE")),
+        selectInput("SourceInput", "Source", c("Prosit", "SpectraST")),
+        numericInput("topX", "Top N Peaks only", 150),
+        numericInput("cutoff", "% intensity cutoff", 0),
+        downloadButton(label = "Generate and Download .db", "downloadData"),
+        add_busy_spinner(spin = "fading-circle", margins = c(80, 300), position='top-left',height="200px",width="200px")
+      )
+    )
+  )
+ )
+
+ui <- dashboardPage(
+        header,
+        side,
+        body
+        )
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -59,7 +80,7 @@ server <- function(input, output) {
     topX = reactive(input$topX)
     cutoff = reactive(input$cutoff)
     output$downloadData <- downloadHandler(
-        filename = function() { paste('library-',Sys.Date(),'.db',sep='') },
+        filename = function() { paste('library-',format(Sys.time(), "%Y-%m-%d_%I-%p"),'.db',sep='') },
         content = function(outFile) { MSPtoDB(Library(),FragmentationMode(), MassAnalyzer(), as.character(CollisionEnergy()), TMTPro(), outFile, Source(), topX(), cutoff()) }
     )
 }
