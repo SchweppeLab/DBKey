90122aac96e7840480a6733c77be5adafa840e3a
ChrisMcGann <cmcgann@uw.edu>
2021-10-11 17:01:54 -0700

    fixed shiny app

--- shiny/app.R
+++ shiny/app.R
@@ -9,49 +9,43 @@
 
 library(shiny)
 
-setwd("~/SchweppeLab/Informatics/MSPtoDB")
-source("lib\\msp2db.R")
+setwd("~/Repos/MSPtoDB")
+#source("lib/blobfunctions.R")
+source("lib/msp2db.R")
+
 
 # Define UI for application that draws a histogram
+options(shiny.maxRequestSize = 30*1024^2)
+
 ui <- fluidPage(
-
+    #MSPtoDB(Library, FragmentationMode, MassAnalyzer, CollisionEnergy, TMTPro, DBoutput, Source)
+    
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+    titlePanel("Library Maker"),
+    fileInput("LibInput", "Input File"),
+    selectInput("FragInput", "Fragmentation mode", c("HCD", "CID")),
+    numericInput("CeInput", "Collision Energy", 35, min =0, max = 00),
+    selectInput("MassAnalyzerInput", "Mass Analyzer", c("IT", "OT")),
+    selectInput("TMTProInput", "Add TMTPro?", c("TRUE", "FALSE")),
+    selectInput("SourceInput", "Source", c("Prosit", "SpectraST")),
+    textInput("DbInput", ".db file name"),
+    actionButton("button", "Go"),
+    add_busy_spinner(spin = "fading-circle")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-        # Library = "DBstuff/yeast_consensus.sptxt"
-        # FragmentationMode = "CID"
-        # MassAnalyzer = "IT"
-        # CollisionEnergy = "35"
-        # Source = "SpectraST"
-        # TMTPro = TRUE
-        #MSPtoDB("DBstuff/yeast_consensus.sptxt", FragmentationMode, MassAnalyzer, CollisionEnergy, TMTPro, "RTLS Databases/YeastTesting54.db", Source)
+    Library = reactive(input$LibInput)
+    FragmentationMode = reactive(input$FragInput)
+    MassAnalyzer =  reactive(input$MassAnalyzerInput)
+    CollisionEnergy = reactive(input$CeInput)
+    Source = reactive(input$SourceInput)
+    TMTPro = reactive(input$TMTProInput)
+    DBoutput = reactive(input$DbInput)
+    
+    observeEvent(input$button, {
+        outputDb<-(MSPtoDB(Library()$datapath, FragmentationMode(), MassAnalyzer(), as.character(CollisionEnergy()), TMTPro(), DBoutput(), Source()))
+        outputDb
     })
 }
 
